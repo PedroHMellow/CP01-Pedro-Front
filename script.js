e7d980ab7e5934f7572cd0722a5f07f9022b2cdd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 09:27:34 -0300

    adicionando checkpoint

---
+++ script.js
@@ -0,0 +1,24 @@
+let nome = "Pedro"; 
+let idade = 20;
+let VB = true;
+let VP = 10.50;
+let time = ('Vasco','São_Paulo','Palmeras');
+
+console.log (nome);
+console.log (idade);
+console.log (VB);
+console.log (VP);
+console.log(time);
+
+let num = 10;
+let num2 = 15;
+
+let soma = num + num2;
+let subtracao = num - num2;
+let multplicacao = num * num2; 
+let divisao = num / num2;
+
+console.log("A soma é:" + soma);
+console.log("A subtração é:" + subtracao);
+console.log("A multploicação é" + multplicacao);
+console.log("A divisão é" + divisao);
